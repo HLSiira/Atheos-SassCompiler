00f5785ca5719a75360b0b0d159e133e233f7325
liam <liam@siira.us>
2020-07-04 16:09:37 -0400

    Updated to match Atheos

---
+++ settings.php
@@ -0,0 +1,16 @@
+<label class="header"><i class="fab fa-sass"></i>Sass Compiler Settings</label>
+<table class="settings">
+	<tr>
+		<td>Format:</td>
+		<td>
+			<select class="setting" data-setting="sass.format">
+				<option value="expanded">Expanded</option>
+				<option value="nested">Nested</option>
+				<option value="compressed" default>Compressed</option>
+				<option value="compact">Compact</option>
+				<option value="crunched">Crunched</option>
+			</select>
+		</td>
+
+	</tr>
+</table>
