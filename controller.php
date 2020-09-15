22d709d736e5bfdb6a1e8c921cfb32fd870fe93b
Liam Siira <liam@siira.us>
2020-09-14 20:16:02 -0400

    Cleaned up a bunch of nonsense

--- controller.php
+++ controller.php
@@ -14,25 +14,17 @@
 require_once "scssphp/scss.inc.php";
 use ScssPhp\ScssPhp\Compiler;
 
-$action = '';
-$path = '';
-
-if (isset($_POST['action']) && isset($_POST['path'])) {
-	$action = $_POST['action'];
-	$path = $_POST['path'];
-} else {
-	die('{"status":"error","message":"missing action/path"}');
-}
+$path = POST("path");
 
 switch ($action) {
 
-	case 'phpCompile':
-		$file = getWorkSpacePath($path);
+	case "phpCompile":
+		$path = Common::getWorkSpacePath($path);
 
-		$pathInfo = pathinfo($file);
+		$pathInfo = pathinfo($path);
 
-		$fullpath = $pathInfo['dirname'];
-		$name = $pathInfo['filename'];
+		$fullpath = $pathInfo["dirname"];
+		$name = $pathInfo["filename"];
 
 		try {
 
@@ -40,66 +32,47 @@ switch ($action) {
 
 			// $scss->setSourceMap(Compiler::SOURCE_MAP_FILE);
 			// $scss->setSourceMapOptions([
-			// 	'sourceMapWriteTo' => "$fullpath/$name.map",
+			// 	"sourceMapWriteTo" => "$fullpath/$name.map",
 			// 	// relative or full url to the above .map file
-			// 	'sourceMapURL' => './',
+			// 	"sourceMapURL" => "./",
 			// ]);
 
 
 			$scss->setImportPaths($fullpath);
-			$scss->setFormatter('ScssPhp\ScssPhp\Formatter\Compressed');
+			$scss->setFormatter("ScssPhp\ScssPhp\Formatter\Compressed");
 
-			// // will search for 'assets/stylesheets/mixins.scss'
-			$content = file_get_contents($file);
+			// // will search for "assets/stylesheets/mixins.scss"
+			$content = file_get_contents($path);
 			$compiled = $scss->compile($content);
 			file_put_contents($fullpath . "/$name.min.css", $compiled);
-			echo '{"status":"success","message":"Compiled to '.$name.'.min.css"}';
+			Common::send("success", "Compiled to $name.min.css");
 
 		} catch (\Exception $e) {
 			$error = $e->getMessage();
-			preg_match('/(?<=line )\d+(?!=,)/i', $error, $line);
-			preg_match('/(?<=column )\d+(?!= )/i', $error, $column);
+			preg_match("/(?<=line )\d+(?!=,)/i", $error, $line);
+			preg_match("/(?<=column )\d+(?!= )/i", $error, $column);
 
-			if (strpos($error, '(stdin)') > 0) {
-				$error = ' in ' . $pathInfo['basename'];
+			if (strpos($error, "(stdin)") > 0) {
+				$error = " in " . $pathInfo["basename"];
 			} else {
-				$remove = getWorkspacePath(pathinfo($path)["dirname"]);
+				$remove = Common::getWorkspacePath(pathinfo($path)["dirname"]);
 				$error = explode($remove, $error)[1];
-				$error = ' in ' . explode(" on line", $error)[0];
+				$error = " in " . explode(" on line", $error)[0];
 			}
 
-			echo json_encode(array(
-				'status' => "error",
-				'message' => $e->getMessage(),
-				'pure' => "Error on Line $line[0]:$column[0]$error"
-			));
+
+			$data = array(
+				"text" => $e->getMessage(),
+				"pure" => "Error on Line $line[0]:$column[0]$error"
+			);
+			
+			Common::send("error", $data);
 		}
 
 
 		break;
 	default:
-		echo '{"status":"error","message":"invalid action"}';
+		Common::send("error", "Invalid action.");
 		break;
 }
-
-function getWorkspacePath($path) {
-	//Security check
-	if (!Common::checkPath($path)) {
-		die('{"status":"error","message":"invalid path"}');
-	}
-	if (strpos($path, "/") === 0) {
-		//Unix absolute path
-		return $path;
-	}
-	if (strpos($path, ":/") !== false) {
-		//Windows absolute path
-		return $path;
-	}
-	if (strpos($path, ":\\") !== false) {
-		//Windows absolute path
-		return $path;
-	}
-	return "../../workspace/".$path;
-}
-
 ?>
